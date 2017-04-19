7972586386defa9db52dc1103d374b66dae5222a
mdouchement <marc.douchement@predicsis.com>
2017-04-19 19:58:12 +0200

    Improved RGB color-space - Add XYZ color-space

--- hdrcolor/color.go
+++ hdrcolor/color.go
@@ -2,6 +2,8 @@ package hdrcolor
 
 import (
 	"image/color"
+
+	colorful "github.com/lucasb-eyer/go-colorful"
 )
 
 // Color can convert itself to alpha-premultiplied 16-bits per channel RGBA and HDR float64 RGB.
@@ -24,11 +26,10 @@ type RGB struct {
 // by a uint32 so that multiplying by a blend factor up to 0xffff will not
 // overflow.
 func (c RGB) RGBA() (r, g, b, a uint32) {
-	// Ugly cast
-	r = uint32(c.R)
-	g = uint32(c.G)
-	b = uint32(c.B)
-	a = 4294967295 // Max uint32
+	r = uint32(c.R * 65535.0)
+	g = uint32(c.G * 65535.0)
+	b = uint32(c.B * 65535.0)
+	a = 0xFFFF
 
 	return
 }
@@ -37,7 +38,36 @@ func (c RGB) RGBA() (r, g, b, a uint32) {
 // for the HDR color.
 func (c RGB) HDRRGBA() (r, g, b, a float64) {
 	r, g, b = c.R, c.G, c.B
-	a = 4294967295.0 // Max uint32 in float64
+	a = 0xFFFF
+
+	return
+}
+
+// XYZ represents a HDR color in XYZ color-space.
+type XYZ struct {
+	X, Y, Z float64
+}
+
+// RGBA returns the alpha-premultiplied red, green, blue and alpha values
+// for the color. Each value ranges within [0, 0xffff], but is represented
+// by a uint32 so that multiplying by a blend factor up to 0xffff will not
+// overflow.
+func (c XYZ) RGBA() (r, g, b, a uint32) {
+	rr, gg, bb, aa := c.HDRRGBA()
+	r = uint32(rr * 65535.0)
+	g = uint32(gg * 65535.0)
+	b = uint32(bb * 65535.0)
+	a = uint32(aa * 65535.0)
+
+	return
+}
+
+// HDRRGBA returns the red, green, blue and alpha values
+// for the HDR color.
+func (c XYZ) HDRRGBA() (r, g, b, a float64) {
+	rgb := colorful.Xyz(c.X, c.Y, c.Z)
+	r, g, b = rgb.R, rgb.G, rgb.B
+	a = 0xFFFF
 
 	return
 }
@@ -45,6 +75,7 @@ func (c RGB) HDRRGBA() (r, g, b, a float64) {
 // Models for the standard color types.
 var (
 	RGBModel color.Model = color.ModelFunc(rgbModel)
+	XYZModel color.Model = color.ModelFunc(xyzModel)
 )
 
 func rgbModel(c color.Color) color.Color {
@@ -62,4 +93,23 @@ func rgbModel(c color.Color) color.Color {
 	// LDR color
 	r, g, b, _ := c.RGBA()
 	return RGB{R: float64(r), G: float64(g), B: float64(b)}
+}
+
+func xyzModel(c color.Color) color.Color {
+	if _, ok := c.(XYZ); ok {
+		// Already XYZ
+		return c
+	}
+
+	if hdrc, ok := c.(Color); ok {
+		// HDR color
+		r, g, b, _ := hdrc.HDRRGBA()
+		x, y, z := colorful.Color{R: r, G: g, B: b}.Xyz()
+		return XYZ{X: x, Y: y, Z: z}
+	}
+
+	// LDR color
+	r, g, b, _ := c.RGBA()
+	x, y, z := colorful.Color{R: float64(r), G: float64(g), B: float64(b)}.Xyz()
+	return XYZ{X: x, Y: y, Z: z}
 }
