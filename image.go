9e89fc527621c392c39f2788a8263be55353908a
mdouchement <marc.douchement@predicsis.com>
2017-04-19 21:59:54 +0200

    Add hdr.XYZ image

--- image.go
+++ image.go
@@ -16,14 +16,11 @@ type Image interface {
 	HDRAt(x, y int) hdrcolor.Color
 }
 
-// NewRGB returns a new HDR RGB image with the given bounds.
-func NewRGB(r image.Rectangle) *RGB {
-	w, h := r.Dx(), r.Dy()
-	buf := make([]float64, 3*w*h)
-	return &RGB{buf, 3 * w, r}
-}
+//===============//
+// RGB           //
+//===============//
 
-// RGB is an in-memory image whose At method returns hdrcolor.RGBE values.
+// RGB is an in-memory image whose At method returns hdrcolor.RGB values.
 type RGB struct {
 	// Pix holds the image's pixels, in R, G, B order. The pixel at
 	// (x, y) starts at Pix[(y-Rect.Min.Y)*Stride + (x-Rect.Min.X)*8].
@@ -32,6 +29,13 @@ type RGB struct {
 	Stride int
 	// Rect is the image's bounds.
 	Rect image.Rectangle
+}
+
+// NewRGB returns a new HDR RGB image with the given bounds.
+func NewRGB(r image.Rectangle) *RGB {
+	w, h := r.Dx(), r.Dy()
+	buf := make([]float64, 3*w*h)
+	return &RGB{buf, 3 * w, r}
 }
 
 // ColorModel implements Image.
@@ -87,4 +91,81 @@ func (p *RGB) SetRGB(x, y int, c hdrcolor.RGB) {
 	p.Pix[i+0] = c.R
 	p.Pix[i+1] = c.G
 	p.Pix[i+2] = c.B
+}
+
+//===============//
+// XYZ           //
+//===============//
+
+// XYZ is an in-memory image whose At method returns hdrcolor.XYZ values.
+type XYZ struct {
+	// Pix holds the image's pixels, in X, Y and Z order. The pixel at
+	// (x, y) starts at Pix[(y-Rect.Min.Y)*Stride + (x-Rect.Min.X)*8].
+	Pix []float64
+	// Stride is the Pix stride (in bytes) between vertically adjacent pixels.
+	Stride int
+	// Rect is the image's bounds.
+	Rect image.Rectangle
+}
+
+// NewXYZ returns a new HDR RGB image with the given bounds.
+func NewXYZ(r image.Rectangle) *XYZ {
+	w, h := r.Dx(), r.Dy()
+	buf := make([]float64, 3*w*h)
+	return &XYZ{buf, 3 * w, r}
+}
+
+// ColorModel implements Image.
+func (p *XYZ) ColorModel() color.Model { return hdrcolor.XYZModel }
+
+// Bounds implements Image.
+func (p *XYZ) Bounds() image.Rectangle { return p.Rect }
+
+// At implements Image.
+func (p *XYZ) At(x, y int) color.Color {
+	return p.XYZAt(x, y)
+}
+
+// HDRAt implements Image.
+func (p *XYZ) HDRAt(x, y int) hdrcolor.Color {
+	return p.XYZAt(x, y)
+}
+
+// XYZAt returns the XYZ color at this coordinate.
+func (p *XYZ) XYZAt(x, y int) hdrcolor.XYZ {
+	if !(image.Point{x, y}.In(p.Rect)) {
+		return hdrcolor.XYZ{}
+	}
+	i := p.PixOffset(x, y)
+	return hdrcolor.XYZ{X: p.Pix[i+0], Y: p.Pix[i+1], Z: p.Pix[i+2]}
+}
+
+// PixOffset returns the index of the first element of Pix that corresponds to
+// the pixel at (x, y).
+func (p *XYZ) PixOffset(x, y int) int {
+	return (y-p.Rect.Min.Y)*p.Stride + (x-p.Rect.Min.X)*3
+}
+
+// Set implements Image.
+func (p *XYZ) Set(x, y int, c color.Color) {
+	if !(image.Point{x, y}.In(p.Rect)) {
+		return
+	}
+	i := p.PixOffset(x, y)
+
+	c1 := hdrcolor.XYZModel.Convert(c).(hdrcolor.XYZ)
+	p.Pix[i+0] = c1.X
+	p.Pix[i+1] = c1.Y
+	p.Pix[i+2] = c1.Z
+}
+
+// SetXYZ applies the given XYZ color at this coordinate.
+func (p *XYZ) SetXYZ(x, y int, c hdrcolor.XYZ) {
+	if !(image.Point{x, y}.In(p.Rect)) {
+		return
+	}
+	i := p.PixOffset(x, y)
+	p.Pix[i+0] = c.X
+	p.Pix[i+1] = c.Y
+	p.Pix[i+2] = c.Z
 }
